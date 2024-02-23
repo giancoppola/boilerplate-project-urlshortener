5b43af529943f88643ed4940f78bb3b04cb9e0a2
Gian <giancoppola@hotmail.co.uk>
2024-02-23 16:36:20 +0000

    url shortener done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +21,33 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let count = 1;
+app.route('/api/shorturl')
+  .post((req, res) => {
+    const protocol_regex = /^\D{3,5}:\/\//;
+    let url = req.body.url ? req.body.url : null;
+    console.log(url)
+    if (protocol_regex.test(url)){
+      SetNewRoute(url, count.toString());
+        res.json({
+          original_url : url,
+          short_url : count.toString()
+        })
+        count++;
+        next();
+    }
+    else{
+      res.json({ error: "invalid url"})
+      next();
+    }
+  })
+
+function SetNewRoute(original, short){
+  app.get(`/api/shorturl/${short}`, (req, res) => {
+    res.redirect(original);
+  })
+}
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
